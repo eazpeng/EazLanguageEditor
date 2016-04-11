2a25f476961531d18feefaae1f5fe3c66d90fa5b
eaz <eazpeng@hotmail.com>
2016-04-12 02:29:53 +0800

    helloworld

---
+++ src/test.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+/**
+ * main function
+ * pdx.160412.org
+ * void
+ * void
+ * infos:
+ *		A hello world file for testing Github.
+**/
+void main(void)
+{
+	printf("Hello Github World!\n");
+}
